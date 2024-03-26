9e097514a852fa2c86f183737e0dd4356c4238b2
cny1213 <cny1213@gmail.com>
2024-03-26 15:35:30 +0900

    [#34] MatchingFragment 추가

---
+++ app/src/main/java/kr/co/lion/mungnolja_nayeon1/Tools.kt
@@ -0,0 +1,13 @@
+package kr.co.lion.mungnolja_nayeon1
+
+class Tools {
+    companion object{
+
+    }
+}
+
+// MainFragment 에서 보여줄 프래그먼트들의 이름
+enum class MainFragmentName(var str:String){
+    MATCHING_FRAGMENT("MatchingFragment"),
+    B_FRAGMENT("B"),
+}
