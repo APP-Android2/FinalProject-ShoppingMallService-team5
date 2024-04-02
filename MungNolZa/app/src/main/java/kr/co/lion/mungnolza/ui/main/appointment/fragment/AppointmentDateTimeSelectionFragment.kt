c979df37d3d5ab3f20079853d5adf56373f207d1
chanho <uni070@naver.com>
2024-04-02 15:42:24 +0900

    [#7] 충돌 해결

--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/main/appointment/fragment/AppointmentDateTimeSelectionFragment.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/main/appointment/fragment/AppointmentDateTimeSelectionFragment.kt
@@ -12,49 +12,28 @@ import com.aminography.primedatepicker.picker.PrimeDatePicker
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
 import kr.co.lion.mungnolza.R
-<<<<<<< HEAD
-import kr.co.lion.mungnolza.databinding.ActivityAppointmentBinding
 import kr.co.lion.mungnolza.databinding.FragmentAppointmentDateTimeSelectionBinding
-import kr.co.lion.mungnolza.ui.main.MainActivity
 import kr.co.lion.mungnolza.ui.main.MainFragmentName
-import kr.co.lion.mungnolza.ui.main.appointment.AppointmentActivity
 import kr.co.lion.mungnolza.ui.main.showAlertDialog
-=======
-import kr.co.lion.mungnolza.databinding.FragmentAppointmentDateTimeSelectionBinding
->>>>>>> origin/7-feature-예약화면-개발
-
 
 class AppointmentDateTimeSelectionFragment : Fragment() {
 
     private var _binding: FragmentAppointmentDateTimeSelectionBinding? = null
     private val binding get() = _binding!!
 
-<<<<<<< HEAD
-    lateinit var appointmentActivity: AppointmentActivity
-
-=======
->>>>>>> origin/7-feature-예약화면-개발
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
 
         _binding = FragmentAppointmentDateTimeSelectionBinding.inflate(inflater)
-<<<<<<< HEAD
-        appointmentActivity = activity as AppointmentActivity
-
 
         setupSelectTimeButton()
         settingupButtons()
         settingDateTimeNextButton()
         settingupDayButton()
-=======
-
         setupSelectTimeButton()
         settingupButtons()
->>>>>>> origin/7-feature-예약화면-개발
 
         return binding.root
     }
-
 
     fun setupSelectTimeButton() {
         // View Binding을 사용하여 뷰에 접근
@@ -109,7 +88,6 @@ class AppointmentDateTimeSelectionFragment : Fragment() {
             // 사용자가 선택한 시간을 EditText에 표시
             val selectedTime = String.format("%02d:%02d", picker.hour, picker.minute)
             binding.editTextVisitTime.setText(selectedTime)
-<<<<<<< HEAD
 
             val checkBoxText = "$selectedTime 로 펫시터님이 협의를 제안합니다"
             binding.checkBoxTime.apply {
@@ -118,18 +96,15 @@ class AppointmentDateTimeSelectionFragment : Fragment() {
             }
 
             // 여기에 CheckBox 변경하는 코드를 추가
-=======
             binding.editTextVisitTime.setText(selectedTime)
 
             // 여기에 CheckBox의 가시성을 변경하는 코드를 추가
->>>>>>> origin/7-feature-예약화면-개발
             binding.checkBoxTime.visibility = View.VISIBLE
             binding.checkBoxCan.visibility = View.VISIBLE
             binding.checkBoxCant.visibility = View.VISIBLE
         }
     }
 
-<<<<<<< HEAD
     fun settingDateTimeNextButton() {
         binding.apply {
             buttonAppointmentCalendarNext.setOnClickListener {
@@ -151,10 +126,6 @@ class AppointmentDateTimeSelectionFragment : Fragment() {
                     return@setOnClickListener
                 }
 
-                // 모든 조건을 충족시키면 다음 화면으로 넘어감
-                appointmentActivity.replaceFragment(
-                    MainFragmentName.APPOINTMENT_REQUEST, true, true, null
-                )
             }
         }
     }
@@ -168,25 +139,15 @@ class AppointmentDateTimeSelectionFragment : Fragment() {
             }
         }
     }
-=======
->>>>>>> origin/7-feature-예약화면-개발
-
     fun calendarSettingForMultipleDates() {
         val today = CivilCalendar()
 
         val datePicker = PrimeDatePicker.bottomSheetWith(today)
             .pickMultipleDays { selectedDays ->
-<<<<<<< HEAD
+
                 val formattedDates = selectedDays.map { day ->
                     "${day.month + 1}월 ${day.dayOfMonth}일"
                 }.joinToString(separator = ", ")
-=======
-                // 여러 날짜 처리 로직
-                val formattedDates = selectedDays.map { day ->
-                    // 각 날짜를 원하는 형식으로 포맷
-                    "${day.month + 1}월 ${day.dayOfMonth}일"
-                }.joinToString(separator = ", ") // 선택된 모든 날짜를 하나의 문자열로 결합
->>>>>>> origin/7-feature-예약화면-개발
 
                 // textViewDateDone에 선택된 날짜들 표시
                 binding.textViewDateDone.text = formattedDates
@@ -197,10 +158,6 @@ class AppointmentDateTimeSelectionFragment : Fragment() {
         datePicker.show(requireActivity().supportFragmentManager, "MULTIPLE_DATE_PICKER_TAG")
     }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/7-feature-예약화면-개발
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
