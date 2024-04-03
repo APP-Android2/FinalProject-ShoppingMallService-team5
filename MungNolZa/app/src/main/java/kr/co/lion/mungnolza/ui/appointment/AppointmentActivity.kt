e32bca5cd5f60cc7b41618b3421f9fc5d3ced322
twentyoo <kritgrit2023@gmail.com>
2024-04-03 14:34:47 +0900

    [#4] 예약 시간 선택 화면 완료

--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/appointment/AppointmentActivity.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/appointment/AppointmentActivity.kt
@@ -10,21 +10,10 @@ import kr.co.lion.mungnolza.databinding.ActivityAppointmentBinding
 
 class AppointmentActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAppointmentBinding
-    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAppointmentBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initView()
-    }
-
-    private fun initView(){
-        navController = this.findNavController(R.id.navHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
     }
 }
 
--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/appointment/fragment/AppointmentDogTimeSelectionFragment.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/appointment/fragment/AppointmentDogTimeSelectionFragment.kt
@@ -1,68 +1,79 @@
 package kr.co.lion.mungnolza.ui.appointment.fragment
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
+import androidx.navigation.Navigation
 import kr.co.lion.mungnolza.R
 import kr.co.lion.mungnolza.databinding.FragmentAppointmentDogTimeSelectionBinding
-import kr.co.lion.mungnolza.ui.main.MainActivity
-import kr.co.lion.mungnolza.ui.main.MainFragmentName
+import kr.co.lion.mungnolza.extensions.setColorGreenBlue
+import kr.co.lion.mungnolza.extensions.setSelectTimeButtonColor
 
 
-class AppointmentDogTimeSelectionFragment : Fragment() {
+class AppointmentDogTimeSelectionFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentAppointmentDogTimeSelectionBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?{
-
         _binding = FragmentAppointmentDogTimeSelectionBinding.inflate(inflater)
-
-        settingupDogTimeButtons()
-
+        initView()
         return binding.root
     }
 
-    private fun settingupDogTimeButtons() {
-        val selectedButtonColor = ContextCompat.getColor(requireContext(), R.color.selectedButtonColor) // 선택된 버튼 색상
-        val defaultButtonColor = ContextCompat.getColor(requireContext(), R.color.defaultButtonColor) // 기본 버튼 색상
+    private fun initView(){
+        with(binding){
+            btnAllDay.setOnClickListener(this@AppointmentDogTimeSelectionFragment)
+            btn30Minutes.setOnClickListener(this@AppointmentDogTimeSelectionFragment)
+            btn1Hour.setOnClickListener(this@AppointmentDogTimeSelectionFragment)
+            btn2Hour.setOnClickListener(this@AppointmentDogTimeSelectionFragment)
+            btnNext.setOnClickListener(this@AppointmentDogTimeSelectionFragment)
+        }
+    }
 
-        binding.apply {
-//            selecttimefordoguntil.setOnClickListener {
-//                // 일반 방문 버튼이 선택되었을 때
-//                it.backgroundTintList = ColorStateList.valueOf(selectedButtonColor)
-//                selecttimefordogthirtymin.backgroundTintList = ColorStateList.valueOf(defaultButtonColor)
-//                selecttimefordogsixtymin.backgroundTintList = ColorStateList.valueOf(defaultButtonColor)
-//                selecttimefordogonetwentymin.backgroundTintList = ColorStateList.valueOf(defaultButtonColor)
-//            }
-//
-//            selecttimefordogthirtymin.setOnClickListener {
-//                // 정기 방문 버튼이 선택되었을 때
-//                it.backgroundTintList = ColorStateList.valueOf(selectedButtonColor)
-//                selecttimefordoguntil.backgroundTintList = ColorStateList.valueOf(defaultButtonColor)
-//                selecttimefordogsixtymin.backgroundTintList = ColorStateList.valueOf(defaultButtonColor)
-//                selecttimefordogonetwentymin.backgroundTintList = ColorStateList.valueOf(defaultButtonColor)
-//            }
-//
-//            selecttimefordogsixtymin.setOnClickListener {
-//                // 정기 방문 버튼이 선택되었을 때
-//                it.backgroundTintList = ColorStateList.valueOf(selectedButtonColor)
-//                selecttimefordoguntil.backgroundTintList = ColorStateList.valueOf(defaultButtonColor)
-//                selecttimefordogthirtymin.backgroundTintList = ColorStateList.valueOf(defaultButtonColor)
-//                selecttimefordogonetwentymin.backgroundTintList = ColorStateList.valueOf(defaultButtonColor)
-//            }
-//
-//            selecttimefordogonetwentymin.setOnClickListener {
-//                // 정기 방문 버튼이 선택되었을 때
-//                it.backgroundTintList = ColorStateList.valueOf(selectedButtonColor)
-//                selecttimefordoguntil.backgroundTintList = ColorStateList.valueOf(defaultButtonColor)
-//                selecttimefordogthirtymin.backgroundTintList = ColorStateList.valueOf(defaultButtonColor)
-//                selecttimefordogsixtymin.backgroundTintList = ColorStateList.valueOf(defaultButtonColor)
-//            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onClick(v: View?) {
+        with(binding){
+            when(v?.id){
+                R.id.btn_all_day -> {
+                    btnAllDay.backgroundTintList = requireContext().setSelectTimeButtonColor()
+
+                    btn30Minutes.backgroundTintList = requireContext().setColorGreenBlue()
+                    btn1Hour.backgroundTintList = requireContext().setColorGreenBlue()
+                    btn2Hour.backgroundTintList = requireContext().setColorGreenBlue()
+                }
+                R.id.btn_30_minutes -> {
+                    btn30Minutes.backgroundTintList = requireContext().setSelectTimeButtonColor()
+
+                    btnAllDay.backgroundTintList = requireContext().setColorGreenBlue()
+                    btn1Hour.backgroundTintList = requireContext().setColorGreenBlue()
+                    btn2Hour.backgroundTintList = requireContext().setColorGreenBlue()
+                }
+                R.id.btn_1_hour -> {
+                    btn1Hour.backgroundTintList = requireContext().setSelectTimeButtonColor()
+
+                    btnAllDay.backgroundTintList = requireContext().setColorGreenBlue()
+                    btn30Minutes.backgroundTintList = requireContext().setColorGreenBlue()
+                    btn2Hour.backgroundTintList = requireContext().setColorGreenBlue()
+                }
+                R.id.btn_2_hour -> {
+                    btn2Hour.backgroundTintList = requireContext().setSelectTimeButtonColor()
+
+                    btnAllDay.backgroundTintList = requireContext().setColorGreenBlue()
+                    btn30Minutes.backgroundTintList = requireContext().setColorGreenBlue()
+                    btn1Hour.backgroundTintList = requireContext().setColorGreenBlue()
+                }
+                R.id.btn_next -> {
+                    val action = AppointmentDogTimeSelectionFragmentDirections.toAppointmentUserAddressFragment()
+                    Navigation.findNavController(v).navigate(action)
+                }
+            }
         }
     }
 }
