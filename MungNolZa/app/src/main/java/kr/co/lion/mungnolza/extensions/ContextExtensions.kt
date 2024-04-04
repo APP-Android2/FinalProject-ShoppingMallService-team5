3d71703460180c5532f5d6f59a85baf0f5f244e0
chanho <uni070@naver.com>
2024-04-04 16:19:32 +0900

    [Chore] 기타 변경 사함 + Context 확장 함수 추가 + 네비게이션 fade animation 추가

--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/extensions/ContextExtensions.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/extensions/ContextExtensions.kt
@@ -1,12 +1,59 @@
 package kr.co.lion.mungnolza.extensions
 
+import android.app.Activity
 import android.content.Context
+import android.content.DialogInterface
+import android.os.SystemClock
 import android.util.TypedValue
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kr.co.lion.mungnolza.R
+import kotlin.concurrent.thread
 
 fun Context.setAppointmentButtonColor() = ContextCompat.getColorStateList(this, R.color.colorPrimary)
+
 fun Context.setColorWhite() = ContextCompat.getColorStateList(this, R.color.white)
 fun Context.setColorPrimary() = ContextCompat.getColorStateList(this, R.color.colorPrimary2)
+fun Context.setSelectTimeButtonColor() = ContextCompat.getColorStateList(this, R.color.v)
+fun Context.setColorGreenBlue() = ContextCompat.getColorStateList(this, R.color.green_blue)
 
+
+// 키보드를 내려주고 포커스를 제거한다.
+fun Context.hideSoftInput(activity: Activity) {
+    // 포커스를 가지고 있는 뷰가 있다면..
+    if (activity.window.currentFocus != null) {
+        // 키보드 관리 객체를 가져온다.
+        val inputMethodManger =
+            activity.getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+        // 키보드를 내려준다.
+        inputMethodManger.hideSoftInputFromWindow(activity.window.currentFocus?.windowToken, 0)
+        // 포커스를 제거해준다.
+        activity.window.currentFocus?.clearFocus()
+    }
+}
+
+// 뷰에 포커스를 주고 키보드를 올린다.
+fun Context.showSoftInput(view: View) {
+    // 뷰에 포커스를 준다.
+    view.requestFocus()
+
+    // 키보드 관리 객체를 가져온다.
+    val inputMethodManger =
+        this.getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+    // 키보드를 올린다.
+    inputMethodManger.showSoftInput(view, 0)
+}
+
+fun Context.showDialog(view: View, title: String, message: String) {
+    val materialAlertDialogBuilder = MaterialAlertDialogBuilder(this)
+    materialAlertDialogBuilder.setTitle(title)
+    materialAlertDialogBuilder.setMessage(message)
+    materialAlertDialogBuilder.setPositiveButton("확인") { dialogInterface: DialogInterface, i: Int ->
+        this.showSoftInput(view)
+    }
+    materialAlertDialogBuilder.show()
+}
 
