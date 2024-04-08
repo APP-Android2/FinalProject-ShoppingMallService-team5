18fb73dd0c55053e4a1086a8fc07a0d183553161
chanho <uni070@naver.com>
2024-04-09 03:00:12 +0900

    [#7] Context 확장 함수 추가

--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ext/ContextExt.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ext/ContextExt.kt
@@ -1,25 +1,21 @@
-package kr.co.lion.mungnolza.extensions
+package kr.co.lion.mungnolza.ext
 
 import android.app.Activity
 import android.content.Context
 import android.content.DialogInterface
-import android.os.SystemClock
-import android.util.TypedValue
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kr.co.lion.mungnolza.R
-import kotlin.concurrent.thread
-
-fun Context.setAppointmentButtonColor() = ContextCompat.getColorStateList(this, R.color.colorPrimary)
 
 fun Context.setColorWhite() = ContextCompat.getColorStateList(this, R.color.white)
+fun Context.setColorBlack() = ContextCompat.getColorStateList(this, R.color.black)
+fun Context.setColorkakaoYellow() = ContextCompat.getColorStateList(this, R.color.kakao_color)
 fun Context.setColorPrimary() = ContextCompat.getColorStateList(this, R.color.colorPrimary2)
 fun Context.setSelectTimeButtonColor() = ContextCompat.getColorStateList(this, R.color.v)
 fun Context.setColorGreenBlue() = ContextCompat.getColorStateList(this, R.color.green_blue)
-
 
 // 키보드를 내려주고 포커스를 제거한다.
 fun Context.hideSoftInput(activity: Activity) {
@@ -45,15 +41,5 @@ fun Context.showSoftInput(view: View) {
         this.getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
     // 키보드를 올린다.
     inputMethodManger.showSoftInput(view, 0)
-}
-
-fun Context.showDialog(view: View, title: String, message: String) {
-    val materialAlertDialogBuilder = MaterialAlertDialogBuilder(this)
-    materialAlertDialogBuilder.setTitle(title)
-    materialAlertDialogBuilder.setMessage(message)
-    materialAlertDialogBuilder.setPositiveButton("확인") { dialogInterface: DialogInterface, i: Int ->
-        this.showSoftInput(view)
-    }
-    materialAlertDialogBuilder.show()
 }
 
