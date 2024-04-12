39d7ae6be3d91e61a63c31b4b7a8f98083c523a3
chanho <uni070@naver.com>
2024-04-12 16:38:44 +0900

    [#33] Home 화면 UI 개발 완료

---
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/model/PetsitterReviewModel.kt
@@ -0,0 +1,19 @@
+package kr.co.lion.mungnolza.model
+
+data class PetsitterReviewModel(
+    // 후기글번호 → reviewIdx
+    // 펫시터 번호 → petsitterIdx
+    // 사용자(작성자) 번호 → reviewWriterIdx
+    // 사용자 이름 → reviewWriterName
+    // 작성날짜 → reviewWriteDate
+    // 별점 → reviewStarCount
+    // 후기내용 → reviewText
+
+    var reviewIdx: Int,
+    /*var petsitterIdx : Int,
+    var reviewWriterIdx : Int,
+    var reviewWriterName : String,*/
+    var reviewWriteDate : String,
+    var reviewStarCount: Float?,
+    var reviewText: String
+)
