6c88cce0f73096ea6e4efb75ccbefb6ce217fa6d
chanho <uni070@naver.com>
2024-04-15 07:00:50 +0900

    [#34] 홈 화면에서 반려 동물 받아오기 + 예약 화면에서 반려동물 정보를 받아올 경우 UI 렌더링에 시간이 걸리기 때문에 홈 화면에서 반려 동물 정보를 받아온 후 Intent로 전달하도록 만들었습니다. + MainViewModel에 petRepository 의존성 및 반려 동물 정보 받아오는 로직 추가

--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/main/viewmodel/MainViewModel.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/main/viewmodel/MainViewModel.kt
@@ -7,14 +7,19 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import kr.co.lion.mungnolza.datasource.MainDataStore
 import kr.co.lion.mungnolza.model.BoardAddUerInfoModel
+import kr.co.lion.mungnolza.model.PetImgModel
 import kr.co.lion.mungnolza.model.UserModel
 import kr.co.lion.mungnolza.repository.freeboard.FreeBoardRepositoryImpl
+import kr.co.lion.mungnolza.repository.pet.PetRepositoryImpl
 import kr.co.lion.mungnolza.repository.user.UserRepositoryImpl
+import java.net.URI
 
 class MainViewModel(
     private val freeBoardRepository: FreeBoardRepositoryImpl,
-    private val userRepository: UserRepositoryImpl
+    private val userRepository: UserRepositoryImpl,
+    private val petRepositoryImpl: PetRepositoryImpl
 ): ViewModel() {
     private val _userList = MutableStateFlow<List<UserModel>>(emptyList())
     val userList = _userList.asStateFlow()
@@ -22,8 +27,21 @@ class MainViewModel(
     private var _boardContentList = MutableStateFlow<List<BoardAddUerInfoModel>>(emptyList())
     val boardContentList = _boardContentList.asStateFlow()
 
+    private val _myUserNumber = MutableStateFlow<String?>(null)
+    private val myUserNumber = _myUserNumber.asStateFlow()
+
+    private val _myPetData = MutableStateFlow<List<PetImgModel>>(emptyList())
+    val myPetData = _myPetData.asStateFlow()
+
     init {
+        viewModelScope.launch {
+            //MainDataStore.setUserNumber("1234")
+            MainDataStore.getUserNumber().collect {
+                _myUserNumber.value = it
+            }
+        }
         fetchAllBoardDataWithUserInfo()
+        fetchMyAllPetData()
     }
 
     private fun fetchAllBoardDataWithUserInfo() = viewModelScope.launch(Dispatchers.IO) {
@@ -39,6 +57,27 @@ class MainViewModel(
             }
         }
         _boardContentList.value = contentList
+    }
+
+    private fun fetchMyAllPetData() = viewModelScope.launch {
+        val petList = ArrayList<PetImgModel>()
+
+        myUserNumber.collect { myUserNumber ->
+            val result = myUserNumber?.let { data-> petRepositoryImpl.fetchMyPetData(data) }
+            result?.map{
+                val imgUri = fetchPetImg(it.ownerIdx, it.imgName)
+                val pet = imgUri?.let { data -> PetImgModel(it, data) }
+
+                if (pet != null) {
+                    petList.add(pet)
+                }
+            }
+            _myPetData.value = petList
+        }
+    }
+
+    private suspend fun fetchPetImg(userIdx: String, petName: String): URI? {
+        return petRepositoryImpl.fetchMyPetImage(userIdx, petName)
     }
     private suspend fun fetchBoardImg(boardIdx: String, imgName: String): Uri? {
         return freeBoardRepository.fetchAllBoardImage(boardIdx, imgName)
