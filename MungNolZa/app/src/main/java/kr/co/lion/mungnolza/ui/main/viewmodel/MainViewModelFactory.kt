a4bc169e73444fe2d590a36c4ea6b503f253e936
chanho <uni070@naver.com>
2024-04-15 07:00:58 +0900

    [#34] 홈 화면에서 반려 동물 받아오기 + 예약 화면에서 반려동물 정보를 받아올 경우 UI 렌더링에 시간이 걸리기 때문에 홈 화면에서 반려 동물 정보를 받아온 후 Intent로 전달하도록 만들었습니다. + MainViewModel에 petRepository 의존성 및 반려 동물 정보 받아오는 로직 추가

--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/main/viewmodel/MainViewModelFactory.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/main/viewmodel/MainViewModelFactory.kt
@@ -3,15 +3,18 @@ package kr.co.lion.mungnolza.ui.main.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import kr.co.lion.mungnolza.repository.freeboard.FreeBoardRepositoryImpl
+import kr.co.lion.mungnolza.repository.pet.PetRepositoryImpl
 import kr.co.lion.mungnolza.repository.user.UserRepositoryImpl
 import java.lang.IllegalArgumentException
 
 class MainViewModelFactory: ViewModelProvider.Factory{
     private val boardRepository = FreeBoardRepositoryImpl()
     private val userRepository = UserRepositoryImpl()
+    private val petRepositoryImpl = PetRepositoryImpl()
+
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MainViewModel::class.java)){
-            return MainViewModel(boardRepository, userRepository) as T
+            return MainViewModel(boardRepository, userRepository, petRepositoryImpl) as T
         }
         throw IllegalArgumentException("unknown ViewModel class")
     }
