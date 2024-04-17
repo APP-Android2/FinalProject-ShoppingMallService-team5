5d733b047257e2403abdd20a4b435e28bc9ce021
cny1213 <cny1213@gmail.com>
2024-04-17 14:08:22 +0900

    [#62] freeboard 뒤로가기 버튼 수정, chat 뒤로가기 버튼 수정

--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/petsitter_main/fragment/PetsitterFreeBoardFragment.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/petsitter_main/fragment/PetsitterFreeBoardFragment.kt
@@ -1,21 +1,81 @@
 package kr.co.lion.mungnolza.ui.petsitter_main.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.divider.MaterialDividerItemDecoration
+import kotlinx.coroutines.launch
 import kr.co.lion.mungnolza.R
 import kr.co.lion.mungnolza.databinding.FragmentPetsitterFreeBoardBinding
+import kr.co.lion.mungnolza.ui.freeboard.AddBoardActivity
+import kr.co.lion.mungnolza.ui.freeboard.BoardActivity
+import kr.co.lion.mungnolza.ui.main.adapter.FreeBoardAdapter
+import kr.co.lion.mungnolza.ui.main.vm.MainViewModel
+import kr.co.lion.mungnolza.ui.main.vm.MainViewModelFactory
 
 class PetsitterFreeBoardFragment : Fragment() {
 
     lateinit var fragmentPetsitterFreeBoardBinding: FragmentPetsitterFreeBoardBinding
 
+    lateinit var freeBoardAdapter: FreeBoardAdapter
+
+    private val viewModel: MainViewModel by activityViewModels { MainViewModelFactory() }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         fragmentPetsitterFreeBoardBinding = FragmentPetsitterFreeBoardBinding.inflate(inflater, container, false)
 
+        initView()
+
         return fragmentPetsitterFreeBoardBinding.root
+    }
+
+    private fun initView() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.boardContentList.collect { boardList ->
+                    val freeBoardAdapter = FreeBoardAdapter(
+                        dataSet = boardList,
+                        onClick = { selectedItemIdx ->
+                            val selectedItem = boardList[selectedItemIdx]
+
+                            val selectedItemData = selectedItem.contentData
+                            val writerData = viewModel.findUserData(selectedItemData.boardWriterIdx)
+
+                            val intent = Intent(requireContext(), BoardActivity::class.java)
+
+                            intent.putExtra("boardData", selectedItemData)
+                            intent.putExtra("userData", writerData)
+
+
+                            startActivity(intent)
+                        }
+                    )
+                    with(fragmentPetsitterFreeBoardBinding.recyclerViewFreeBoard) {
+                        adapter = freeBoardAdapter
+                        layoutManager = LinearLayoutManager(requireContext())
+                        val deco = MaterialDividerItemDecoration(
+                            requireContext(),
+                            com.google.android.material.divider.MaterialDividerItemDecoration.VERTICAL
+                        )
+                        addItemDecoration(deco)
+                    }
+                }
+            }
+        }
+
+        fragmentPetsitterFreeBoardBinding.buttonAddBoardFreeBoard.setOnClickListener{
+            val intent = Intent(requireContext(), AddBoardActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
