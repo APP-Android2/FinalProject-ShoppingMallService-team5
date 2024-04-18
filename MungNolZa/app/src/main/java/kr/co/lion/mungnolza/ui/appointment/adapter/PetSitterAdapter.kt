c724343cb4d0f0320803ce8d5c4635cc5e67a5bc
chanho <uni070@naver.com>
2024-04-18 13:51:37 +0900

    [#84] 펫시터 RecyclerView 수정
    + 리뷰 개수를 클릭하면 펫시터 리뷰 화면으로 이동
    + 젠체 항목을 클릭하면 전체 배경화면 변경

--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/appointment/adapter/PetSitterAdapter.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/appointment/adapter/PetSitterAdapter.kt
@@ -1,40 +1,44 @@
 package kr.co.lion.mungnolza.ui.appointment.adapter
 
 import android.content.Context
-import android.graphics.Color
 import android.text.SpannableString
 import android.text.style.UnderlineSpan
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import kr.co.lion.mungnolza.R
 import kr.co.lion.mungnolza.databinding.RowMatchingBinding
 import kr.co.lion.mungnolza.model.PetSitterModelWithImg
 
-
 class PetSitterAdapter(
     private val petSitters: ArrayList<PetSitterModelWithImg>,
-    private val itemClick: (Int) -> Unit
+    private val reviewCntClick: (Int) -> Unit,
+    private val itemClick: (Int) -> Unit,
 ): RecyclerView.Adapter<PetSitterAdapter.PetSitterViewHolder>() {
     private var selectedItemPosition: Int = RecyclerView.NO_POSITION
 
     inner class PetSitterViewHolder(
         private val context: Context,
         private val binding: RowMatchingBinding,
+        private val reviewCntClick: (Int) -> Unit,
         private val itemClick: (Int) -> Unit
     ): RecyclerView.ViewHolder(binding.root){
 
         init {
 
             binding.reviewCnt.setOnClickListener {
+                reviewCntClick.invoke(adapterPosition)
+            }
+
+            binding.root.setOnClickListener{
                 if (adapterPosition != RecyclerView.NO_POSITION) {
                     notifyItemChanged(selectedItemPosition) // 기존 선택된 아이템 배경색 변경
                     selectedItemPosition = adapterPosition
                     notifyItemChanged(selectedItemPosition) // 새로운 선택된 아이템 배경색 변경
                 }
-
                 itemClick.invoke(adapterPosition)
-
             }
         }
 
@@ -58,14 +62,25 @@ class PetSitterAdapter(
                     .load(item.profileImg.toString())
                     .into(imgPetSitter)
 
-                itemView.setBackgroundColor(if (adapterPosition == selectedItemPosition) Color.YELLOW else Color.TRANSPARENT)
+                root.backgroundTintList = if (adapterPosition == selectedItemPosition){
+                        ContextCompat.getColorStateList(context, R.color.app_main_color)
+                    } else{
+                        ContextCompat.getColorStateList(context, R.color.white)
+                    }
+
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PetSitterViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        return PetSitterViewHolder(parent.context, RowMatchingBinding.inflate(inflater, parent, false), itemClick)
+
+        return PetSitterViewHolder(
+            parent.context,
+            RowMatchingBinding.inflate(inflater, parent, false),
+            reviewCntClick,
+            itemClick
+        )
     }
 
     override fun getItemCount() = petSitters.size
