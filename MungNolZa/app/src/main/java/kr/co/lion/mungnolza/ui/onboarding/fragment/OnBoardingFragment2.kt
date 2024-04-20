dcb47b310e341827c6598a5b84d36022a486a2a6
chanho <uni070@naver.com>
2024-04-20 09:29:26 +0900

    [#89] 온보딩 Fragment bind 구조 변경

--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/onboarding/fragment/OnBoardingFragment2.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/onboarding/fragment/OnBoardingFragment2.kt
@@ -1,24 +1,6 @@
 package kr.co.lion.mungnolza.ui.onboarding.fragment
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import kr.co.lion.mungnolza.R
-import kr.co.lion.mungnolza.databinding.FragmentOnBoarding2Binding
 
-
-class OnBoardingFragment2 : Fragment() {
-    private var _binding: FragmentOnBoarding2Binding? = null
-    private val binding get() = _binding!!
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        _binding = FragmentOnBoarding2Binding.inflate(layoutInflater)
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-}
+class OnBoardingFragment2 : Fragment(R.layout.fragment_on_boarding2)
--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/onboarding/fragment/OnBoardingFragment3.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/onboarding/fragment/OnBoardingFragment3.kt
@@ -2,38 +2,25 @@ package kr.co.lion.mungnolza.ui.onboarding.fragment
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.launch
+import kr.co.lion.mungnolza.R
 import kr.co.lion.mungnolza.databinding.FragmentOnBoarding3Binding
 import kr.co.lion.mungnolza.ui.main.MainActivity
-import kr.co.lion.mungnolza.ui.main.vm.MainViewModel
-import kr.co.lion.mungnolza.ui.main.vm.MainViewModelFactory
 import kr.co.lion.mungnolza.ui.onboarding.vm.OnBoardingViewModel
 import kr.co.lion.mungnolza.ui.onboarding.vm.OnBoardingViewModelFactory
 
-class OnBoardingFragment3 : Fragment() {
-    private var _binding: FragmentOnBoarding3Binding? = null
-    private val binding get() = _binding!!
+class OnBoardingFragment3 : Fragment(R.layout.fragment_on_boarding3) {
     private val viewModel: OnBoardingViewModel by activityViewModels { OnBoardingViewModelFactory() }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        _binding = FragmentOnBoarding3Binding.inflate(layoutInflater)
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initView()
-    }
-
-    private fun initView(){
+        val binding = FragmentOnBoarding3Binding.bind(view)
         binding.btnNext.setOnClickListener {
             viewLifecycleOwner.lifecycleScope.launch {
                 repeatOnLifecycle(Lifecycle.State.STARTED) {
@@ -45,10 +32,5 @@ class OnBoardingFragment3 : Fragment() {
                 }
             }
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
