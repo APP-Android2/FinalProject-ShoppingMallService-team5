22cd1b47dff6af9d47cc6adb28c03c7a8059d378
chanho <uni070@naver.com>
2024-04-21 23:43:35 +0900

    [#98] LoginViewModel petRepository 의존성 추가

--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/intro/vm/LoginViewModelFactory.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/intro/vm/LoginViewModelFactory.kt
@@ -2,13 +2,16 @@ package kr.co.lion.mungnolza.ui.intro.vm
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import kr.co.lion.mungnolza.repository.pet.PetRepositoryImpl
 import kr.co.lion.mungnolza.repository.user.UserRepositoryImpl
 
 class LoginViewModelFactory: ViewModelProvider.Factory {
     private val userRepositoryImpl = UserRepositoryImpl()
+    private val petRepositoryImpl = PetRepositoryImpl()
+
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(LoginViewModel::class.java)){
-            return LoginViewModel(userRepositoryImpl) as T
+            return LoginViewModel(userRepositoryImpl, petRepositoryImpl) as T
         }
         return super.create(modelClass)
     }
