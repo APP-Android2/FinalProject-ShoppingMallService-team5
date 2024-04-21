90e8d1afe6d911e80eb10f5d9597c196c9bb6be9
chanho <uni070@naver.com>
2024-04-21 15:41:55 +0900

    [#98] StartViewModel 펫시터 정보 받기 완료

--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/intro/activity/StartActivity.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/intro/activity/StartActivity.kt
@@ -4,18 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kr.co.lion.mungnolza.R
 import kr.co.lion.mungnolza.databinding.ActivityStartBinding
 import kr.co.lion.mungnolza.ui.dialog.RequestPermissionDialog
 import kr.co.lion.mungnolza.ui.intro.vm.StartViewModel
+import kr.co.lion.mungnolza.ui.intro.vm.StartViewModelFactory
 import kr.co.lion.mungnolza.ui.main.MainActivity
 
 class StartActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStartBinding
-    private val viewModel: StartViewModel by viewModels()
+    private val viewModel: StartViewModel by viewModels{ StartViewModelFactory() }
     private val dialog = RequestPermissionDialog(
         buttonClick = {
             startActivity(Intent(this, LoginActivity::class.java))
@@ -37,8 +40,16 @@ class StartActivity : AppCompatActivity() {
             if (!viewModel.checkFistFlag()){
                 dialog.show(supportFragmentManager, "RequestPermissionDialog")
             }else{
-                startActivity(Intent(this@StartActivity, MainActivity::class.java))
-                finish()
+                lifecycleScope.launch {
+                    repeatOnLifecycle(Lifecycle.State.STARTED) {
+                        viewModel.myPetData.collect {
+                            val intent = Intent(this@StartActivity, MainActivity::class.java)
+                            intent.putExtra("myPet", it.toTypedArray())
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+                }
             }
         }
     }
--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/intro/vm/StartViewModel.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/intro/vm/StartViewModel.kt
@@ -1,9 +1,44 @@
 package kr.co.lion.mungnolza.ui.intro.vm
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import kr.co.lion.mungnolza.datasource.MainDataStore
+import kr.co.lion.mungnolza.model.PetImgModel
+import kr.co.lion.mungnolza.repository.pet.PetRepositoryImpl
+import java.net.URI
 
-class StartViewModel: ViewModel() {
+class StartViewModel(
+    private val petRepositoryImpl: PetRepositoryImpl
+): ViewModel() {
+
+    private val _myPetData = MutableStateFlow<List<PetImgModel>>(emptyList())
+    val myPetData = _myPetData.asStateFlow()
+
+    init {
+        fetchMyAllPetData()
+    }
+    private fun fetchMyAllPetData() = viewModelScope.launch {
+        val petList = ArrayList<PetImgModel>()
+        MainDataStore.getUserNumber().collect { myUserNumber ->
+            val result = myUserNumber.let { data-> petRepositoryImpl.fetchMyPetData(data) }
+            result.map{
+                val imgUri = fetchPetImg(it.ownerIdx, it.imgName)
+                val pet = imgUri?.let { data -> PetImgModel(it, data) }
+
+                if (pet != null) {
+                    petList.add(pet)
+                }
+            }
+            _myPetData.value = petList
+        }
+    }
+
+    private suspend fun fetchPetImg(userIdx: String, petName: String): URI? {
+        return petRepositoryImpl.fetchMyPetImage(userIdx, petName)
+    }
     suspend fun checkFistFlag(): Boolean{
         return MainDataStore.getFirstFlag()
     }
