981115de3590c5ff252ca3b9739c6e6497b541ad
chanho <uni070@naver.com>
2024-04-21 15:24:33 +0900

    [#98] 온보딩 화면 ViewModel 제거

--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/onboarding/fragment/OnBoardingFragment1.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/onboarding/fragment/OnBoardingFragment1.kt
@@ -1,18 +1,6 @@
 package kr.co.lion.mungnolza.ui.onboarding.fragment
 
-import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import kr.co.lion.mungnolza.R
-import kr.co.lion.mungnolza.ui.onboarding.vm.OnBoardingViewModel
-import kr.co.lion.mungnolza.ui.onboarding.vm.OnBoardingViewModelFactory
 
-class OnBoardingFragment1 : Fragment(R.layout.fragment_on_boarding1) {
-    private val viewModel: OnBoardingViewModel by activityViewModels { OnBoardingViewModelFactory() }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        viewModel.fetchMyAllPetData()
-    }
-}
+class OnBoardingFragment1 : Fragment(R.layout.fragment_on_boarding1)
