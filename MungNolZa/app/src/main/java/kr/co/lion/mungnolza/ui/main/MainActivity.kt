b20d71cfc8a042106ef4d8215c70628a17632b64
chanho <uni070@naver.com>
2024-04-24 02:23:26 +0900

    [#99] Merge Conflict Resolve

--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/main/MainActivity.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/main/MainActivity.kt
@@ -14,12 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
-
-    private fun initView(){
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
         NavigationUI.setupWithNavController(binding.navigationView, navController)
-
     }
 }
