1fd60f69a02d6b11fee2807abe92d720bddb0396
cny1213 <cny1213@gmail.com>
2024-04-25 09:47:51 +0900

    [#122]commit

--- MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/reservation_list/viewmodel/PetSitterReviewWriteViewModel.kt
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/reservation_list/viewmodel/PetSitterReviewWriteViewModel.kt
@@ -6,6 +6,12 @@ import androidx.lifecycle.ViewModel
 // 펫시터 후기 작성
 
 class PetSitterReviewWriteViewModel : ViewModel() {
+    // 펫시터 이름
+    // 펫시터 프로필 사진
+    // 펫시터 별점 (숫자)
+    // 펫시터 별점 (래이팅바)
+    // 후기 개수
+
     // 별점 → starCount
     val ratingBar2 = MutableLiveData<Float>()
     // 후기내용 → ReviewText
