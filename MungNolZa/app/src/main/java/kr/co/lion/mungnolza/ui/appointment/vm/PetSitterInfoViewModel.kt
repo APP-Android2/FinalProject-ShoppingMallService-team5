ddd4a76d0e2cc34f75489e424a5d8225bc6f3325
chanho <uni070@naver.com>
2024-04-26 09:46:57 +0900

    [#126] 펫시터 정보 ViewModel 및 사용

---
+++ MungNolZa/app/src/main/java/kr/co/lion/mungnolza/ui/appointment/vm/PetSitterInfoViewModel.kt
@@ -0,0 +1,43 @@
+package kr.co.lion.mungnolza.ui.appointment.vm
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kr.co.lion.mungnolza.model.ReviewAddUserInfoModel
+import kr.co.lion.mungnolza.repository.petsitter.PetSitterRepository
+import kr.co.lion.mungnolza.repository.review.ReviewRepositoryImpl
+import kr.co.lion.mungnolza.repository.user.UserRepositoryImpl
+import java.net.URI
+
+class PetSitterInfoViewModel(
+    private val userRepository: UserRepositoryImpl,
+    private val reviewRepository: ReviewRepositoryImpl,
+    private val petSitterRepository: PetSitterRepository,
+): ViewModel() {
+
+    private val _review = MutableStateFlow<List<ReviewAddUserInfoModel>>(emptyList())
+    val review = _review.asStateFlow()
+
+    fun onLoadingPetSitterPage(petSitterIdx: Int) = viewModelScope.launch{
+        val result = ArrayList<ReviewAddUserInfoModel>()
+        val response = reviewRepository.fetchPetSitterReview(petSitterIdx)
+
+        response.map { review->
+            val user = userRepository.fetchUserData(review.reviewWriterIdx.toString())
+            user?.let {
+                result.add(ReviewAddUserInfoModel(
+                    review,
+                    URI.create(user.userProfileImgPath)
+                ))
+            }
+        }
+        _review.value = result
+    }
+
+
+    suspend fun fetchPetSitterImage(petSitterIdx: String, imgName: String): URI {
+        return petSitterRepository.fetchPetSitterImage(petSitterIdx, imgName)
+    }
+}
